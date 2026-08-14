3d329c440dfe1d7b904bf14693fe397fa98fe44a
pawlopawlo123 <rojewskipawel123@gmail.com>
2026-08-14 09:21:49 +0200

    Update index.html

--- service-woker.js
+++ service-woker.js
@@ -1,19 +1,57 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("rozklad-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json"
-      ]);
+const CACHE_NAME = "rozklad-cache-v2";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      );
     })
   );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+
 });
